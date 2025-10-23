e904c5eade5f9aaf79fcb77053051e257a4660a2
duy nguyen <duybnguyen04@gmail.com>
2025-10-22 21:48:55 -0400

    Add Product class: - added attributes matching that of a product - generated constructor and getters for all attributes

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
 public class Product {
+    String sku;
+    String productName;
+    double price;
+    String department;
+
+    public Product(String department, double price, String productName, String sku) {
+        this.department = department;
+        this.price = price;
+        this.productName = productName;
+        this.sku = sku;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getSku() {
+        return sku;
+    }
 }
